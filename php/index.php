ddc005fca26ccd188386e4d1aa513e5c334c7e2a
paulixe <pchaumont@ensc.fr>
2022-04-27 13:15:44 +0200

    aa

---
+++ php/index.php
@@ -0,0 +1,57 @@
+<?php
+require_once('connect.php')
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/style.css" rel="stylesheet">
+    <title> Team E-Sport</title>
+</head>
+
+<body>
+    <header>
+        <h1> Team E-Sport
+        </h1>
+    </header>
+
+    <main>
+        <h2>
+            Etapes suivies pour le projet
+        </h2>
+        <ul>
+            <li>
+                Regarder l'état de la salle
+            </li>
+            <li>
+                Essayer de lancer League Of Legends
+            </li>
+            <li>
+                Faire le cahier des charges
+            </li>
+            <li>
+                envoyer un formulaire aux joueurs intéressés
+            </li>
+            <li>
+                Faire la commande Dell
+            </li>
+            <li>
+                Reunir les joueurs
+            </li>
+            <li>
+                Faire des livrables pour mieux organiser les équipes
+            </li>
+            <li>
+                Verifier nos hypothèses
+            </li>
+            <li>
+                Installer les PC
+            </li>
+        </ul>
+    </main>
+
+</body>
+
+</html>
