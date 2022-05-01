09aafdbc568d8bf95922e971b90addd353ce5f1e
paulixe <pchaumont@ensc.fr>
2022-05-01 13:08:34 +0200

    a

--- fichiersPHP/index.php
+++ fichiersPHP/index.php
@@ -19,34 +19,34 @@ require_once('connect.php')
 
     <main>
         <div id="jalonContainer">
-            <div class="jalon">
+            <a class="jalon" href="#">
                 <h3>Regarder l'état de la salle</h3>
-            </div>
+            </a>
 
-            <div class="jalon">
+            <a class="jalon" href="#">
             <h3>Essayer de lancer League Of Legends</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3>Faire le cahier des charges</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3> Envoyer un formulaire aux joueurs intéressés</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3>Faire la commande Dell</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3> Reunir les joueurs</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3> Faire des livrables pour mieux organiser les équipes</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3> Verifier nos hypothèses</h3>
-            </div>
-            <div class="jalon">
+</a>
+            <a class="jalon" href="#">
             <h3>Installer les PC</h3>
-            </div>
+</a>
         </div>
     </main>
 
