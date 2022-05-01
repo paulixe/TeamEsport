09087de406a6f96a59fadb7813b391baf69381d0
paulixe <pchaumont@ensc.fr>
2022-05-01 13:05:22 +0200

    cadre

--- fichiersPHP/index.php
+++ fichiersPHP/index.php
@@ -20,50 +20,34 @@ require_once('connect.php')
     <main>
         <div id="jalonContainer">
             <div class="jalon">
+                <h3>Regarder l'état de la salle</h3>
+            </div>
 
+            <div class="jalon">
+            <h3>Essayer de lancer League Of Legends</h3>
             </div>
             <div class="jalon">
-
+            <h3>Faire le cahier des charges</h3>
             </div>
             <div class="jalon">
-
-</div>
-<div class="jalon">
-
-</div>
+            <h3> Envoyer un formulaire aux joueurs intéressés</h3>
+            </div>
+            <div class="jalon">
+            <h3>Faire la commande Dell</h3>
+            </div>
+            <div class="jalon">
+            <h3> Reunir les joueurs</h3>
+            </div>
+            <div class="jalon">
+            <h3> Faire des livrables pour mieux organiser les équipes</h3>
+            </div>
+            <div class="jalon">
+            <h3> Verifier nos hypothèses</h3>
+            </div>
+            <div class="jalon">
+            <h3>Installer les PC</h3>
+            </div>
         </div>
-        <ol>
-            <h2>
-                Etapes suivies pour le projet
-            </h2>
-            <li>
-                Regarder l'état de la salle
-            </li>
-            <li>
-                Essayer de lancer League Of Legends
-            </li>
-            <li>
-                Faire le cahier des charges
-            </li>
-            <li>
-                envoyer un formulaire aux joueurs intéressés
-            </li>
-            <li>
-                Faire la commande Dell
-            </li>
-            <li>
-                Reunir les joueurs
-            </li>
-            <li>
-                Faire des livrables pour mieux organiser les équipes
-            </li>
-            <li>
-                Verifier nos hypothèses
-            </li>
-            <li>
-                Installer les PC
-            </li>
-            </ul>
     </main>
 
 </body>
