c99ca6ed34c93146aa347b3af196c418d02864c3
paulixe <pchaumont@ensc.fr>
2022-05-15 11:44:24 +0200

    images

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('connect.php')
+require_once('fichiersPHP/connect.php')
 ?>
 <!doctype html>
 <html>
@@ -7,7 +7,7 @@ require_once('connect.php')
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <title> Team E-Sport</title>
 </head>
 
@@ -20,33 +20,35 @@ require_once('connect.php')
     <main>
         <div id="jalonContainer">
             <a class="jalon" href="#">
+                <img src="images/ordinateur.jpg" alt="image d'ordinateur">
                 <h3>Regarder l'état de la salle</h3>
             </a>
-
+            
             <a class="jalon" href="#">
-            <h3>Essayer de lancer League Of Legends</h3>
-</a>
-            <a class="jalon" href="#">
+            <img src="images/cahier_des_charges.jpg" alt="image de cahier des charges">
             <h3>Faire le cahier des charges</h3>
 </a>
             <a class="jalon" href="#">
+            <img src="images/formulaire.jpg" alt="image de cahier des charges">
             <h3> Envoyer un formulaire aux joueurs intéressés</h3>
 </a>
             <a class="jalon" href="#">
+            <img src="images/Dell.jpg" alt="image de cahier des charges">
             <h3>Faire la commande Dell</h3>
 </a>
             <a class="jalon" href="#">
+            <img src="images/travail_équipe.jpg" alt="image de cahier des charges">
             <h3> Reunir les joueurs</h3>
 </a>
             <a class="jalon" href="#">
+            <img src="images/livrables.jpg" alt="image de cahier des charges">
             <h3> Faire des livrables pour mieux organiser les équipes</h3>
 </a>
             <a class="jalon" href="#">
+            <img src="images/validification.jpg" alt="image de cahier des charges">
             <h3> Verifier nos hypothèses</h3>
 </a>
-            <a class="jalon" href="#">
-            <h3>Installer les PC</h3>
-</a>
+
         </div>
     </main>
 
